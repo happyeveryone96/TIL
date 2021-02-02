24063dca9a8788a366d18fe959d962bfadd52cc8
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-02-02 22:34:02 +0900

    Update function.js

--- JavaScript/Function/function.js
+++ JavaScript/Function/function.js
@@ -146,3 +146,21 @@ const simpleMultiply = (a,b) => {
 (function hello() {
     console.log('IIFE');
 })();
+
+function calculate(command, a, b) {
+    switch (command) {
+      case 'add':
+        return a + b;
+      case 'substract':
+        return a - b;
+      case 'divide':
+        return a / b;
+      case 'multiply':
+        return a * b;
+      case 'remainder':
+        return a % b;
+      default:
+        throw Error('unkonwn command');
+    }
+  }
+  console.log(calculate('add', 2, 3));
