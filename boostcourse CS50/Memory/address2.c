14a0a0fc5b08b35b407252b6f23a03035b03e67f
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-02-16 22:51:33 +0900

    Create address2.c

---
+++ boostcourse CS50/Memory/address2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "EMMA";
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%c\n", *(s+3));
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+}
