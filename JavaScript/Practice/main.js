d23029cf2217bfcb6e9e517510cdaed460a55e0b
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-02-18 22:44:23 +0900

    Update main.js

--- JavaScript/Practice/main.js
+++ JavaScript/Practice/main.js
@@ -1,1 +1,42 @@
-console.log('Im working. Im JS. Im Beautiful, Im worth it');
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    title.classList.toggle(CLICKED_CLASS);
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(hasClass) {
+    //     title.classList.remove(CLICKED_CLASS);
+    // } else {
+    //     title.classList.add(CLICKED_CLASS);
+    // }
+}
+
+// const BASE_COLOR = "white";
+// const OTHER_COLOR = "#7f8c8d";
+
+// function handleClick() {
+//     const currentColor = title.style.color;
+//     if (currentColor === BASE_COLOR) {
+//         title.style.color = OTHER_COLOR;
+//     } else {
+//         title.style.color = BASE_COLOR;
+//     }
+// }
+
+function init() {
+    // title.addEventListener("mouseenter", handleClick);
+    title.addEventListener("click", handleClick);
+}
+init();
+
+// function handleOffline() {
+//     console.log('Bye bye');
+// }
+
+// function handleOnline() {
+//     console.log('Welcome back');
+// }
+
+// window.addEventListener("offline", handleOffline);
+// window.addEventListener("online", handleOnline);
